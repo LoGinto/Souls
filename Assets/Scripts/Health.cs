a4df032ec93cc287aef973b26ca011fc29b80d4b
LoGinto <46216287+LoGinto@users.noreply.github.com>
2020-08-28 14:44:00 +0400

    Dodge rolling almost complete

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float healthPoints;
+    private float invisibilityAmount;
+    // Update is called once per frame
+
+
+    public void TakeDamage(float damage)
+    {
+        if (invisibilityAmount <= 0)
+        {
+            healthPoints -= damage;
+        }
+    }
+    void Update()
+    {
+        if (invisibilityAmount > 0)
+        {
+            invisibilityAmount -= Time.deltaTime;
+        }
+    }
+    public void Invinsible(float delay,float invLength)
+    {
+        if (delay > 0)
+        {
+            StartCoroutine(StartInvisible(delay,invLength));
+        }
+        else
+        {
+            invisibilityAmount = invLength;
+        }
+    }
+
+    IEnumerator StartInvisible(float delay,float invLength)
+    {
+        yield return new WaitForSeconds(delay);
+        invisibilityAmount = invLength;
+    }
+}
--- Assets/Scripts/Locomotion.cs
+++ Assets/Scripts/Locomotion.cs
@@ -7,8 +7,15 @@ public class Locomotion : MonoBehaviour
     Animator animator;
     Camera myCamera;
     [SerializeField] float speed = 2f;
+    [SerializeField] float delayBeforeInvinsible = 0.1f;
+    [SerializeField] float invisibleDuration = 1f;
     CharacterController characterController;
     Melee melee;
+    [SerializeField] float coolDown = 1f;
+    float activeCooldown;
+    bool isRollin;
+    Health health;
+    bool space;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +23,7 @@ public class Locomotion : MonoBehaviour
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         melee = GetComponent<Melee>();
+        health = GetComponent<Health>();
     }
 
     // Update is called once per frame
@@ -23,9 +31,19 @@ public class Locomotion : MonoBehaviour
     {
         if (melee.GetNumberOfClicks() == 0)
         {
-            Move();
+            if (!isRollin)
+            {
+                Move();
+            }
+            Roll();  
         }
-       
+       space = Input.GetKey(KeyCode.Space);
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Stand To Roll"))
+        {
+            AnimatorStateInfo currInfo = animator.GetCurrentAnimatorStateInfo(0);
+            health.Invinsible(delayBeforeInvinsible,currInfo.normalizedTime-delayBeforeInvinsible);
+        }
+
     }
     void Move()
     {
@@ -39,9 +57,51 @@ public class Locomotion : MonoBehaviour
         Vector3 actualMovement = updatedVector * speed * Time.deltaTime;
         animator.SetBool("Run", true);
         characterController.Move(actualMovement);//actual movement
-        if (verticalAxis == 0 && horizontalAxis == 0)//if player doesn't move
+        if (verticalAxis == 0 && horizontalAxis == 0 || isRollin)//if player doesn't move
         {           
             animator.SetBool("Run", false);
+        }
+    }
+    void Roll()
+    {
+        Camera kamera = myCamera;
+        float verticalAxis = Input.GetAxis("Vertical");//varies between 1 and -1
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Vector3 cameraForward = Vector3.Scale(kamera.transform.forward, new Vector3(1, 0, 1)).normalized; // forward direction in 2d
+        Vector3 updatedVector = verticalAxis * cameraForward + horizontalAxis * kamera.transform.right;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isRollin = true;            
+            animator.SetBool("Run", false);
+            if (activeCooldown <= 0)
+            {
+                if (!(this.animator.GetCurrentAnimatorStateInfo(0).IsName("Stand To Roll")))
+                {
+                    if (verticalAxis != 0 || horizontalAxis != 0)
+                    {
+                        animator.SetTrigger("Roll");
+                        transform.LookAt(updatedVector + transform.position);
+                    }
+                }
+                
+            }
+            else
+            {
+                activeCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            isRollin = false;
+        }
+        
+    }
+    
+    public void RollAnim()
+    {
+        if (space)
+        {
+            animator.SetTrigger("Roll");
         }
     }
 }
